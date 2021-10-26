286f3bb76b329503ac40432cac1e6f1b69f3d876
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-26 10:48:44 -0400

    add regex test

--- seed.f90
+++ seed.f90
@@ -14,10 +14,10 @@ call random_seed(put=seed)
 
 call random_number(r)
 
-print '(3F12.6)', r
+print '(3F9.6)', r
 
-! Intel,IntelLLVM: 0.000000    0.025480    0.352516
-! GNU:    0.471078    0.117346    0.884884
+! Intel,IntelLLVM: 0.000000 0.025480 0.352516
+! GNU: 0.471078 0.117346 0.884884
 
 
 end program
