e021da7b20f74028970bbab43cfaca7188adb76f
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-26 10:07:20 -0400

    add matlab,python

---
+++ seed.py
@@ -0,0 +1,9 @@
+import numpy.random
+import numpy as np
+
+# r = numpy.random.default_rng(seed=0)
+r = numpy.random.Generator(numpy.random.MT19937(seed=0))
+
+print(np.array2string(r.standard_normal(3), precision=6)[1:-1])
+
+# Twister MT19937: -0.625151 -0.596945  0.139097
